c38e75c91f82be68dc6790857ce46b175c44a21c
= <=>
2024-05-22 16:54:57 +0800

    提交

--- src/view/components/node.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, memo } from 'react'
-import { VNode } from '~/handle/state'
-import { Flex } from '../widget/flex'
-
-export const NodeComp: FC<{ node: VNode; type: 'old' | 'new'; activeIndex: number }> = memo(
-  ({ type, node, activeIndex }) => {
-    return <Flex className='lay-v wh-100-200 b-1-black'></Flex>
-  },
-)
